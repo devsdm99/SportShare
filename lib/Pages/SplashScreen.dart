f8c42f5f6437c869541de632432b9df78d15b996
sergio diaz marcos <xexidiaz@hotmail.com>
2019-02-27 20:39:57 +0100

    funcionando login y register

--- lib/Pages/SplashScreen.dart
+++ lib/Pages/SplashScreen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Timer(
         Duration(seconds: 4),
-        () => Navigator.push(context,
+        () => Navigator.pushReplacement(context,
             MaterialPageRoute(builder: (context) => LoginPage())));
   }
 
