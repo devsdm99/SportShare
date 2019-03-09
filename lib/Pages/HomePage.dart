9cf5e954ced2ae7b7a138e51383b1251cbf7cb39
djsdm99 <djsdm99@gmail.com>
2019-03-09 22:20:55 +0000

    Update HomePage.dart

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import '../BackgroundGradient.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomePage extends StatefulWidget
-{
-  const HomePage({Key key, this.user}) : super(key: key);
+class HomePage extends StatelessWidget{
+  const HomePage({Key key, this.user}):super(key:key);
   final FirebaseUser user;
-  @override
-  _HomeState createState() => _HomeState();
   
-  }
-  
-  class _HomeState extends State<HomePage> 
-  {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home ${widget.user.email}'),
+        title: Text('Home ${user.email}'),
+      ),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: Firestore.instance.collection('users').document(user.uid).snapshots(),
+        
       ),
     );
   }
+
 }
